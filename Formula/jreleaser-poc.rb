2152c276dd1798df29d1e50eed3fb56214ee3158
jreleaserbot <jreleaser@kordamp.org>
2022-01-27 11:15:48 +0000

    jreleaser-poc v0.2.5

--- Formula/jreleaser-poc.rb
+++ Formula/jreleaser-poc.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.0.0-SNAPSHOT at 2022-01-27T10:39:14.07928752Z
+# Generated with JReleaser 1.0.0-SNAPSHOT at 2022-01-27T11:15:34.023064823Z
 class JreleaserPoc < Formula
   desc "PoC for JReleaser"
   homepage "https://github.com/manojkarthick/jreleaser-poc"
@@ -7,19 +7,19 @@ class JreleaserPoc < Formula
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/manojkarthick/jreleaser-poc/releases/download/v0.2.5/jreleaser-poc-0.2.5-aarch64-unknown-linux-gnu.zip"
-    sha256 "7595c40df3b340b3da78b7b50fec1ea98f3ca350217b080ca0ac4669b50d874c"
+    sha256 "8c29c7b0df125a8c8297d3108382ea141043f894b1ca99e4eacea3a2ff71cb4d"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/manojkarthick/jreleaser-poc/releases/download/v0.2.5/jreleaser-poc-0.2.5-x86_64-unknown-linux-gnu.zip"
-    sha256 "45e57d6016ccc64bc6b6b277ac53a44cca39c71df67955493532a9e619166bd5"
+    sha256 "f5c87c203eea8788ac55349b397557a453f78a5c21f04e812b62a4a7fe5bae25"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/manojkarthick/jreleaser-poc/releases/download/v0.2.5/jreleaser-poc-0.2.5-aarch64-apple-darwin.zip"
-    sha256 "59bfaa35d14deb958004c997d935f0d022532b8f5e2d7da2b43936a2a6696ae6"
+    sha256 "30f631d1178e0ead29afbced74b9b3acd64321934297efd10452ac29fef0cb9a"
   end
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/manojkarthick/jreleaser-poc/releases/download/v0.2.5/jreleaser-poc-0.2.5-x86_64-apple-darwin.zip"
-    sha256 "acd08e351e57b1db848e7b6fa263c4eda10a9453ee32c864748470341258ec55"
+    sha256 "652d9ae237b2fce3a4469c11733edd3a1308bbac401f4b04252befd86f75a166"
   end
 
 
